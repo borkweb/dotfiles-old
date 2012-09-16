7a8d04ccc94175f1bf478e3ec62ae247ef773d35
Matthew Batchelder <borkweb@gmail.com>
2012-09-16 01:01:52 -0400

    Add brew file and set osx defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-# Sets reasonable OS X defaults.
+###############################################################################
+# General UI/UX
+###############################################################################
 
-# Always open everything in Finder's list view. This is important.
-defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
-# Show the ~/Library folder.
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+###############################################################################
+# Finder
+###############################################################################
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Show the ~/Library folder
 chflags nohidden ~/Library
