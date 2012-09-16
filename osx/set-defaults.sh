fde59ae05f61c634cf868420b6faad829630841c
Matthew Batchelder <borkweb@gmail.com>
2012-09-16 00:28:59 -0400

    Adding osx directory and moving git stuff

---
+++ osx/set-defaults.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Sets reasonable OS X defaults.
+
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
